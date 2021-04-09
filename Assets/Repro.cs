4e53caa5f58650a24c3e4ca2956a27936f363d11
Ondřej Nepožitek <ondra@nepozitek.cz>
2021-04-09 15:44:35 +0200

    Initial commit

---
+++ Assets/Repro.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Edgar.GraphBasedGenerator.Grid2D;
+using UnityEngine;
+
+public class Repro : MonoBehaviour
+{
+    void Start()
+    {
+        // Make sure that the DLL is referenced
+        var generator = new GraphBasedGeneratorGrid2D<int>(null);
+    }
+
+    void Update()
+    {
+        
+    }
+}
